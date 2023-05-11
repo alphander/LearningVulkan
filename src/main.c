1b66bd375eee126969e2e7bad9f34d3793fb3665
BuildTools <unconfigured@null.spigotmc.org>
2023-05-11 03:44:26 +0100

    Queue added

--- src/main.c
+++ src/main.c
@@ -30,8 +30,6 @@ int main()
 	// ####################################################################################################
 	// VkInstance
 
-	VkInstance instance;
-
 	VkApplicationInfo applicationInfo = {};
 	applicationInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
 	applicationInfo.applicationVersion = VK_MAKE_VERSION(1, 0, 0);
@@ -48,8 +46,10 @@ int main()
 	instanceCreateInfo.enabledLayerCount = enabledLayerCount;
 	instanceCreateInfo.ppEnabledLayerNames = enabledLayerArray;
 
+	VkInstance instance;
+
 	result = vkCreateInstance(&instanceCreateInfo, NULL, &instance);
-	if (result != VK_SUCCESS) error("Problem at vkCreateInstance! VkResult: %d\n", result); // ERROR HANDLING
+	if (result != VK_SUCCESS) error("Problem at vkCreateInstance! VkResult: %s\n", result_to_name(result)); // ERROR HANDLING
 
 	// ####################################################################################################
 	// VkPhysicalDevice
@@ -59,7 +59,7 @@ int main()
 
 	VkPhysicalDevice physicalDeviceArray[physicalDeviceCount];
 	result = vkEnumeratePhysicalDevices(instance, &physicalDeviceCount, physicalDeviceArray);
-	if (result != VK_SUCCESS) error("Problem at vkEnumeratePhysicalDevices! VkResult: %d\n", result); // ERROR HANDLING
+	if (result != VK_SUCCESS) error("Problem at vkEnumeratePhysicalDevices! VkResult: %s\n", result_to_name(result)); // ERROR HANDLING
 
 	// ####################################################################################################
 	// VkPhysicalDeviceProperties
@@ -73,49 +73,66 @@ int main()
 		vkGetPhysicalDeviceFeatures(physicalDeviceArray[i], &physicalDeviceFeaturesArray[i]);
 	}
 
-	// ----------------------------------------------------------------------------------------------------
-	// Logging "physicalDevicePropertiesArray"
-	#ifdef LOG_ALL_VULKAN_DEBUG
+	printf("\nPhysical Device List:\n");
+	printf("-------------------------------------------------------------------------\n");
+	for (int i = 0; i < physicalDeviceCount; i++)
 	{
-		printf("\nPhysical Device List:\n");
-		printf("-------------------------------------------------------------------------\n");
-		for (int i = 0; i < physicalDeviceCount; i++)
+		VkPhysicalDeviceProperties physicalDeviceProperties = physicalDevicePropertiesArray[i]; 
+		VkPhysicalDeviceFeatures physicalDeviceFeatures = physicalDeviceFeaturesArray[i];
+		printf("  %s:\n", physicalDeviceProperties.deviceName);
+		printf("    - Device Type: \"%s\"\n", physical_device_type_to_name(physicalDeviceProperties.deviceType));
+		printf("\n");
+	}
+
+	// ####################################################################################################
+	// VkPhysicalDevice selection
+
+	VkPhysicalDevice physicalDevice = NULL;
+	uint32_t queueFamilyIndex = 0;
+
+	printf("\nQueue Family:\n");
+	printf("-------------------------------------------------------------------------");
+	for (int i = 0; i < physicalDeviceCount; i++)
+	{
+		printf("\n -Queue Families: %s\n", physicalDevicePropertiesArray[i].deviceName);
+		VkPhysicalDeviceProperties currentPhysicalDeviceProperties = physicalDevicePropertiesArray[i];
+
+		uint32_t currentQueueFamilyPropertiesCount;
+		vkGetPhysicalDeviceQueueFamilyProperties(physicalDeviceArray[i], &currentQueueFamilyPropertiesCount, NULL);
+		VkQueueFamilyProperties currentQueueFamilyPropertiesArray[physicalDeviceCount];
+		vkGetPhysicalDeviceQueueFamilyProperties(physicalDeviceArray[i], &currentQueueFamilyPropertiesCount, currentQueueFamilyPropertiesArray);
+
+		for (int j = 0; j < currentQueueFamilyPropertiesCount; j++)
 		{
-			VkPhysicalDeviceProperties physicalDeviceProperties = physicalDevicePropertiesArray[i]; 
-			VkPhysicalDeviceFeatures physicalDeviceFeatures = physicalDeviceFeaturesArray[i];
-			printf("  %s:\n", physicalDeviceProperties.deviceName);
-			printf("    - Device Type: \"%s\"\n", physical_device_type_to_name(physicalDeviceProperties.deviceType));
+			VkQueueFamilyProperties currentQueueFamilyProperties = currentQueueFamilyPropertiesArray[j];
+			VkQueueFlags queueFlags = currentQueueFamilyProperties.queueFlags;
+
+			printf("   -Queue Family Index %i:", j);
+
+			if (currentQueueFamilyProperties.queueFlags & VK_QUEUE_COMPUTE_BIT &&
+				currentPhysicalDeviceProperties.deviceType == VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU)
+			{
+				printf(" VALID\n");
+				physicalDevice = physicalDeviceArray[i];
+				queueFamilyIndex = j;
+			}
+			else printf(" NOT VALID\n");
+
+			uint32_t flagCount;
+			queue_flags_to_name(queueFlags, &flagCount, NULL);
+			char* queueFlagNameArray[flagCount];
+			queue_flags_to_name(queueFlags, &flagCount, queueFlagNameArray);
+
+
+			for (int k = 0; k < flagCount; k++)
+			{
+				printf("      %s\n", queueFlagNameArray[k]);
+			}
 			printf("\n");
 		}
 	}
-	#endif
 
-	VkPhysicalDevice physicalDevice;
-
-	for (int i = 0; i < physicalDeviceCount; i++)
-	{
-		VkPhysicalDeviceProperties currentPhysicalDeviceProperties = physicalDevicePropertiesArray[i];
-		if (currentPhysicalDeviceProperties.deviceType != VK_PHYSICAL_DEVICE_TYPE_DISCRETE_GPU) 
-			continue;
-
-		uint32_t currentQueueFamilyPropertiesCount;
-		vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &currentQueueFamilyPropertiesCount, NULL);
-
-		VkQueueFamilyProperties currentQueueFamilyPropertiesArray[currentQueueFamilyPropertiesCount];
-		vkGetPhysicalDeviceQueueFamilyProperties(physicalDevice, &currentQueueFamilyPropertiesCount, currentQueueFamilyPropertiesArray);
-
-		bool complete = false;
-		for (int j = 0; j < currentQueueFamilyPropertiesCount; j++)
-		{
-			if (currentQueueFamilyPropertiesArray[j].queueFlags & VK_QUEUE_COMPUTE_BIT)
-			{
-				physicalDevice = physicalDeviceArray[i];
-				complete = true;
-				break;
-			}
-		}
-		if (complete) break;
-	}
+	if (physicalDevice == NULL) error("No valid queues!");
 
 	// ####################################################################################################
 	// vkDevice
@@ -124,16 +141,20 @@ int main()
 	deviceQueueCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_QUEUE_CREATE_INFO;
 	deviceQueueCreateInfo.queueCount = 1;
 	deviceQueueCreateInfo.pQueuePriorities = (float[1]) {1.0f};
-	deviceQueueCreateInfo.queueFamilyIndex = 0;
-
-	VkDevice device;
+	deviceQueueCreateInfo.queueFamilyIndex = queueFamilyIndex;
 
 	VkDeviceCreateInfo deviceCreateInfo = {};
 	deviceCreateInfo.sType = VK_STRUCTURE_TYPE_DEVICE_CREATE_INFO;
+	deviceCreateInfo.pQueueCreateInfos = &deviceQueueCreateInfo;
+	deviceCreateInfo.queueCreateInfoCount = 1;
 
+	VkDevice device;
+	result = vkCreateDevice(physicalDevice, &deviceCreateInfo, NULL, &device);
+	if (result != VK_SUCCESS) error("Problem at vkCreateDevice! VkResult: %s\n", result_to_name(result)); // ERROR HANDLING
 
+	VkQueue queue;
+	vkGetDeviceQueue(device, queueFamilyIndex, 0, &queue);
 
-	vkCreateDevice(physicalDevice, &deviceCreateInfo, NULL, &device);
 
 	// ####################################################################################################
 	//	Cleanup
